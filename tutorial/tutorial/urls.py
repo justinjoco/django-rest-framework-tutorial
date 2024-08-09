0fda94ad891ec9c52f5312287ba4cf1bcb3f6b6e
Justin Joco <justin.joco@gmail.com>
2024-08-08 22:52:32 -0700

    uses in depth tutorial

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for rest_tutorial project.
+URL configuration for tutorial project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,16 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from tutorial.quickstart import views
-
-router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet)
-router.register(r"groups", views.GroupViewSet)
 
 urlpatterns = [
-    path("", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework"))
+    path('', include('snippets.urls')),
 ]
